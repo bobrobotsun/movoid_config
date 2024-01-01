0e77e39f4174d04d06de92ab16ac9cc83b57cbd4
movoid <bobrobotsun@163.com>
2024-01-01 12:22:09 +0800

    2024.01.01.01-bobrobotsun 1、初次创建

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='movoid_config',
+    version='1.0.0',
+    packages=['movoid_config'],
+    url='',
+    license='',
+    author='movoid',
+    author_email='bobrobotsun@163.com',
+    description=''
+)
