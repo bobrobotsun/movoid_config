58451b20b0a43904c21307ddca0933fbcccaaab0
movoid <bobrobotsun@163.com>
2024-01-07 20:34:00 +0800

    2024.01.07.03-bobrobotsun 1、增加了setup的一些规则

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name='movoid_config',
     version='1.0.0',
@@ -8,5 +11,8 @@ setup(
     license='',
     author='movoid',
     author_email='bobrobotsun@163.com',
-    description=''
+    description='',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[],
 )
