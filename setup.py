a9ee4f960cef2788a99ea4c2b29f4bda254e17aa
movoid <bobrobotsun@163.com>
2024-01-08 23:22:28 +0800

    2024.01.08.01-bobrobotsun 1、修改了打包逻辑

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(
     name='movoid_config',
-    version='1.1.1',
-    packages=['movoid_config'],
+    version='1.1.2',
+    packages=find_packages(),
     url='',
     license='',
     author='movoid',
